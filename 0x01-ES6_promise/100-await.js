dfd8bd248fecd773ca7721464642115a80c89d42
foo <foo@bar.com>
2024-06-27 02:04:06 +0100

    promise was fun

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+    let objt;
+
+    try{
+        objt = {photo: await uploadPhoto(), user: await createUser()};
+    }catch(error){
+        objt = {
+            photo: null,
+            user: null,
+          };
+    }
+
+    return objt;
+}
